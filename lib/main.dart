56a5127e5f998b2ebe18e188e753fb8856432b79
Marek Sicinski <marek_tadeusz.sicinski@mailbox.tu-dresden.de>
2023-12-15 20:53:02 +0100

    added some fonts

--- lib/main.dart
+++ lib/main.dart
@@ -10,41 +10,46 @@ class UniPlanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         toolbarHeight: 50,
         title: const Text(
-          'MyUniPlanner',
+          'MyUNI-Planner',
         style: TextStyle(
-          color: Colors.white
+          color: Colors.white,
+          fontWeight: FontWeight.bold
           ),
         ),
         centerTitle: true,
-        backgroundColor: Colors.blue[800],
+        backgroundColor: Colors.blue[900],
         elevation: 0.0,
       ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
-              'Meine Daten',
+              'Personal Data',
               style: TextStyle(
-                color: Colors.blue[800],
-                fontSize: 22.0,
+                color: Colors.blue[900],
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              'Matriculation Nr.: 1234567',
+              style: TextStyle(
+                color: Colors.blue[900],
+                fontSize: 12.0,
               ),
             ),
             Text(
-              'Matrikel Nummer: 123456789',
+              'S-Nr.: 1234567',
               style: TextStyle(
-                color: Colors.blue[800],
-                fontSize: 16.0,
-              ),
-            ),
-            Text(
-              'S.- Nummer: 123456789',
-              style: TextStyle(
-                color: Colors.blue[800],
-                fontSize: 16.0,
+                color: Colors.blue[900],
+                fontSize: 12.0,
               ),
             ),
           ],
